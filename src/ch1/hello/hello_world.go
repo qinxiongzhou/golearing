74504fb7763ef21fe63529243d79b929f62f60ec
qinxiongzhou <qinxiongzhou@126.com>
2021-03-19 22:14:11 +0800

    首次提交

---
+++ src/ch1/hello/hello_world.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Hello ")
+	if len(os.Args) > 1 {
+		fmt.Println(os.Args[1])
+	}
+	os.Exit(0)
+
+}
---
+++ src/ch2/test/first_test.go
@@ -0,0 +1,7 @@
+package try_test
+
+import "testing"
+
+func TestFisrtTry(t *testing.T) {
+	t.Log("My first try!")
+}
