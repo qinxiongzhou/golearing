4a49620fca3dd3e191b1795380b4b2b331b09615
qinxiongzhou <363606635@qq.com>
2021-06-22 23:13:39 +0800

    测试按比例分配算法

--- src/ch8/map/mapext_test.go
+++ src/ch8/map/mapext_test.go
@@ -29,3 +29,56 @@ func TestMapForSet(t *testing.T) {
 		t.Log(key + " is not exist")
 	}
 }
+
+func TestUint32(t *testing.T) {
+	myMap := make(map[uint32]uint32)
+	myMap[111111]=45
+	myMap[222222]=53
+	myMap[333333]=62
+
+	var sum uint32 = 0
+
+	for _, v := range myMap {
+		sum = sum + v
+	}
+	
+	var couponNum uint32 = 20
+	
+	count1 := myMap[111111] *couponNum/sum
+	count2 := myMap[222222] *couponNum/sum
+	count3 := couponNum - count1 - count2
+
+
+	t.Log(count1)
+	t.Log(count2)
+	t.Log(count3)
+	t.Log(count1 + count2 + count3)
+}
+
+func TestInt(t *testing.T) {
+
+	myMap := make(map[int]int)
+	myMap[111111]=45
+	myMap[222222]=53
+	myMap[333333]=62
+
+	var sum int = 0
+
+	for _, v := range myMap {
+		sum = sum + v
+	}
+
+	var couponNum int = 4
+
+
+	count1 := myMap[111111] *couponNum/sum
+	count2 := myMap[222222] *couponNum/sum
+
+	count3 := couponNum - count1 - count2
+
+
+	t.Log(count1)
+	t.Log(count2)
+	t.Log(count3)
+	t.Log(count1 + count2 + count3)
+}
